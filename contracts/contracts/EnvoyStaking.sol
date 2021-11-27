012809f73af8219cdf528cad53136255d22a42b9
Kasper De Blieck <KasperDeBlieck@hotmail.com>
2021-11-27 21:12:56 +0100

    v0.0.1

--- contracts/contracts/EnvoyStaking.sol
+++ contracts/contracts/EnvoyStaking.sol
@@ -1,34 +1,45 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/IERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract EnovyStaking is ERC20, Ownable {
+contract EnvoyStaking is Ownable {
 
-    constructor(){}
+    using SafeMath for uint;
 
-    struct ShareHolder {
-        uint staked;
+    event Staking(address indexed stakeholder_, uint stake_);
+    event Rewarding(address indexed stakeholder_, uint reward_);
+
+    struct StakeHolder {
+        uint stakingBalance;
         uint weight;
-        uint lastUpdate; 
+        uint interestDate; 
+        uint newStake; 
+        uint newWeight;
     }
 
-    mapping(address => ShareHolder) shareholders;
+    mapping(address => StakeHolder) public stakeholders;
 
-    uint cooldown = 2 days;
+    uint totalStake;
+    uint maxWeight;
+
+    uint public cooldown = 2 days;
     
+    address wallet;
     address signatureAddress;
 
     IERC20 stakingToken;
 
+    uint public interestDecimals = 1000000000000;
+    uint public baseInterest = 3924000000;
+    uint public extraInterest = 39240000000; //10% per year
+    uint public compoundingPeriod = 15 days;
 
-
-    constructor(address signatureAddress_, address stakingTokenAddress) ERC20('ENVOY staking token', 'ENV-S'){
+    constructor(address signatureAddress_, address stakingTokenAddress) {
         wallet = _msgSender();
         signatureAddress = signatureAddress_;
         stakingToken = IERC20(stakingTokenAddress);
@@ -43,49 +54,141 @@ contract EnovyStaking is ERC20, Ownable {
         signatureAddress = signatureAddress_; 
     }
 
-    function updateWeight(uint weight_, bytes signature) public{
+    function updateWeight(uint weight_, bytes memory signature, bool instant) public{
         address sender = _msgSender();
         require(signatureAddress == _recoverSigner(sender, weight_, signature),
             "Signature of the input was not signed by 'signatureAddress'");
 
-        stakeholder.getNewRewards(sender);        
-        stakeholders[sender].weight = weight_;
-
+        StakeHolder storage stakeholder = stakeholders[sender];
+        if(instant){
+            claimRewards(false);
+            stakeholder.interestDate = block.timestamp;
+            stakeholder.weight = weight_;
+        } else {
+            stakeholder.newWeight = weight_;
+        }
     }
 
-    function stake(uint amount) public {
+    function stake(uint amount, bool instant) public {
         address sender = _msgSender();
 
-        require(stakingToken.allowance(sender, address(this) >= amount),
+        require(stakingToken.allowance(sender, address(this)) >= amount,
             "The staking contract is not approved to stake this amount");
 
-        stakingToken.safeTransferFrom(sender, address(this), amount);
+        stakingToken.transferFrom(sender, address(this), amount);
 
-        stakeholder = stakeholders[sender];
-        stakeholder.staked += amount;
-        stakeholder.getNewRewards(sender);
+        StakeHolder storage stakeholder = stakeholders[sender];
 
+        if(instant){
+            claimRewards(false);
+            stakeholder.interestDate = block.timestamp;
+            stakeholder.stakingBalance += amount;
+        } else {
+            claimRewards(false);
+            stakeholder.newStake = amount;
+        }
+
+        totalStake += amount;
+
+        emit Staking(sender, amount);
 
     }
 
-    function withdrawl(uint amount) public payable {
+    function withdrawlFunds(uint amount) public payable {
+        address sender = _msgSender();
+        StakeHolder storage stakeholder = stakeholders[sender];
+        require(stakeholder.interestDate + cooldown < block.timestamp, "Funds are locked until cooldown period is over");
         
+        claimRewards(false);
+        
+        require(stakeholder.stakingBalance >= 0, "Nothing was staked");
+        
+        if(amount > stakeholder.stakingBalance){
+            amount = stakeholder.stakingBalance;
+        }
+        stakingToken.transfer(sender, stakeholder.stakingBalance);
+
+        totalStake -= amount;
     }
 
-    function getNewRewards(address stakeholder) internal view returns(uint){
-        stakeholder.lastUpdate = now();
+    /**
+     * Calculate the rewards owed to 
+     */
+    function claimRewards(bool withdrawl) public returns(uint reward) {
+
+        address stakeholderAddress = _msgSender();
+        StakeHolder storage stakeholder = stakeholders[stakeholderAddress];
+
+        
+        // Number of accounts for which rewards will be paid
+        uint n = (block.timestamp-stakeholder.interestDate) / compoundingPeriod;
+
+        if (stakeholder.stakingBalance == 0 || n == 0){
+            return 0;
+        }
+
+        if (stakeholder.newWeight > 0 || stakeholder.newStake > 0){
+            // If updates were scheduled for the next period:
+            // - first calculate rewards on the first compounding period with the old values
+            // - set the new values to use in the computation of the following compounding periods
+            stakeholder.stakingBalance += stakeholder.stakingBalance * (baseInterest + extraInterest * stakeholder.weight);
+            stakeholder.interestDate += compoundingPeriod;
+
+            if(stakeholder.newWeight > 0){
+                stakeholder.weight = stakeholder.newWeight;
+                stakeholder.newWeight = 0;
+            }
+
+            if(stakeholder.newStake > 0){
+                stakeholder.stakingBalance += stakeholder.newStake;
+                stakeholder.newStake = 0;
+            }
+
+            stakeholder.interestDate += compoundingPeriod;
+            
+            // One period was already rewarded
+            n-=1;
+
+        }
+
+        // Update the timestamp of the timestamp for the staking period that was not rewarded yet
+        stakeholder.interestDate += (n * compoundingPeriod);
+
+        uint s = stakeholder.stakingBalance;
+        uint r = baseInterest + extraInterest * stakeholder.weight;
+
+        while (n > 0) {
+            s += s * r / interestDecimals;
+            n -= 1;
+        }
+
+        reward = s - stakeholder.stakingBalance;
+
+        // If the stakeholder wants to redraw the rewards;
+        // Send to his wallet. Else, update stakingbalance.
+        if (withdrawl){
+            stakingToken.transfer(_msgSender(), reward);
+        } else {
+            stakeholder.stakingBalance = s;
+            totalStake += reward;
+        }
+
+        emit Rewarding(stakeholderAddress, reward);
+
     }
+
+
 
 
     /**
      * Checks if the signature is created out of the contract address, sender and new weight,
      * signed by the private key of the signerAddress
      * @param sender the address of the message sender
-     * @param amount amount of tokens to mint
+     * @param weight amount of tokens to mint
      * @param signature a signature of the contract address, senderAddress and tokensId.
      *   Should be signed by the private key of signerAddress.
      */
-    function _recoverSigner(address sender, uint256 weight, bytes memory signature) public view returns (address){
+    function _recoverSigner(address sender, uint weight, bytes memory signature) public view returns (address){
         return ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256(abi.encode(address(this), sender, weight))) , signature);
     }
 
--- contracts/migrations/1_initial_migration.js
+++ contracts/migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
-const Migrations = artifacts.require("Migrations");
+const EnvoyStaking = artifacts.require("EnvoyStaking");
+const TestToken = artifacts.require("TestToken");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+
+module.exports = async function (deployer, network, accounts) {
+  if(network === 'development'){
+    // Deploy a test token 
+    const signatureAddress = '0xb9DD1FBbeB8f29DD181223DA36A397859B02834C' //29087b7eef74b57abe95659609151eeebac31ab7018a2a995a2f3bc1fdda89e3
+    await deployer.deploy(TestToken)
+    const token = await TestToken.deployed()
+    for(var account in accounts){
+      token.claim(accounts[account], web3.utils.toWei('100'))
+    }
+    await deployer.deploy(EnvoyStaking, signatureAddress, token.address);
+  }
 };
