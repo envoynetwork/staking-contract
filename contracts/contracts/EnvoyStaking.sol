cc84d1c18481061ec952dceb0b8cb5a5a44c37f4
Kasper De Blieck <KasperDeBlieck@hotmail.com>
2021-11-28 12:11:18 +0100

    minor changes and docs

--- contracts/contracts/EnvoyStaking.sol
+++ contracts/contracts/EnvoyStaking.sol
@@ -4,13 +4,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+/**
+ * @title A staking contract for Envoy tokens
+ * @author Kasper De Blieck (kasperdeblieck@envoy.art)
+ * This contract allows Envoy token owners to stake their funds.
+ * Staking funds will reward a periodic compounded interest.
+ */
 contract EnvoyStaking is Ownable {
-
+    
     using SafeMath for uint;
 
+    event ConfigUpdate(string field, uint value);
     event Staking(address indexed stakeholder_, uint stake_);
     event Rewarding(address indexed stakeholder_, uint reward_);
 
@@ -23,46 +29,36 @@ contract EnvoyStaking is Ownable {
     }
 
     mapping(address => StakeHolder) public stakeholders;
-
-    uint totalStake;
-    uint maxWeight;
-
-    uint public cooldown = 2 days;
     
-    address signatureAddress;
+    address public signatureAddress;
 
-    IERC20 stakingToken;
+    IERC20 public stakingToken;
 
+    // Decimals used for interest calculation
     uint public interestDecimals = 1000000000000;
     uint public baseInterest = 4000000000;
     uint public extraInterest = 400000000; //10% per year
     uint public interestPeriod = 15 days;
+    uint public cooldown = 2 days;
+
+    uint totalStake;
+    uint maxWeight;
 
     constructor(address signatureAddress_, address stakingTokenAddress) {
         signatureAddress = signatureAddress_;
         stakingToken = IERC20(stakingTokenAddress);
     }
 
-    function updateSignatureAddress(address value) public onlyOwner {
-        signatureAddress = value; 
-    }
-
-    function updateInterestDecimals(uint value) public onlyOwner{
-        interestDecimals = value;
-    }
-
-    function updateBaseInterest(uint value) public onlyOwner{
-        baseInterest = value;
-    }
-
-    function updateExtraInterest(uint value) public onlyOwner{
-        extraInterest = value;
-    }
-
-    function updateInterestPeriod(uint value) public onlyOwner{
-        interestPeriod = value;
-    }
-
+    /**
+     * Increase the stake of the sender by a value.
+     * @param weight_ The new weight.
+     * @param signature A signature proving the sender
+     *  is allowed to update his weight.
+     * @param instant if false, finish current period with
+     *  old values and start new period with new value.
+     *  If true, end current period without rewards and
+     *  start a new period with the new values.
+     */
     function updateWeight(uint weight_, bytes memory signature, bool instant) public{
         address sender = _msgSender();
         require(signatureAddress == _recoverSigner(sender, weight_, signature),
@@ -70,7 +66,9 @@ contract EnvoyStaking is Ownable {
 
         StakeHolder storage stakeholder = stakeholders[sender];
 
+        // Claim previous rewards with old weight
         claimRewards(false);
+
         if(instant){
             stakeholder.interestDate = block.timestamp;
             stakeholder.weight = weight_;
@@ -78,14 +76,25 @@ contract EnvoyStaking is Ownable {
             stakeholder.newWeight = weight_;
         }
 
+        // Keep track of highest weight
         if(weight_ > maxWeight){
             maxWeight = weight_;
         }
     }
 
+    /**
+     * Increase the stake of the sender by a value.
+     * @param amount The amount to stake
+     * @param instant if false, finish current period with
+     *  old values and start new period with new value.
+     *  If true, end current period without rewards and
+     *  start a new period with the new values.
+     *  If the current stake is 0, this should be instant.
+     */
     function stake(uint amount, bool instant) public {
         address sender = _msgSender();
 
+        require(amount > 0, "Staking requires positive value");
         require(stakingToken.allowance(sender, address(this)) >= amount,
             "The staking contract is not approved to stake this amount");
 
@@ -93,42 +102,60 @@ contract EnvoyStaking is Ownable {
 
         StakeHolder storage stakeholder = stakeholders[sender];
 
+        // Claim previous rewards with old staked value
         claimRewards(false);
         if(instant){
             stakeholder.interestDate = block.timestamp;
             stakeholder.stakingBalance += amount;
         } else {
+            require(stakeholder.stakingBalance > 0, "New stakers must stake instantly");
             stakeholder.newStake = amount;
         }
 
+        // Keep track of the total stake in the contract
         totalStake += amount;
 
         emit Staking(sender, amount);
 
     }
 
-    function withdrawlFunds(uint amount) public payable {
+    /**
+     * Withdraw staked funds from the contract.
+     * This will calculate the owed rewards for previous periods,
+     * the current interest period will end without reward.
+     * @param amount The amount to withdraw, capped by stakingbalance
+     */
+    function withdrawFunds(uint amount) public {
         address sender = _msgSender();
         StakeHolder storage stakeholder = stakeholders[sender];
+
         require(stakeholder.interestDate + cooldown < block.timestamp, "Funds are locked until cooldown period is over");
-        
-        claimRewards(false);
-        
         require(stakeholder.stakingBalance >= 0, "Nothing was staked");
+        
+        // Claim rewards with current stake
+        claimRewards(false);
         
         if(amount > stakeholder.stakingBalance){
             amount = stakeholder.stakingBalance;
         }
-        stakingToken.transfer(sender, stakeholder.stakingBalance);
+        stakingToken.transfer(sender, amount);
 
         stakeholder.interestDate = block.timestamp;
         stakeholder.stakingBalance -= amount;
-
         totalStake -= amount;
     }
 
     /**
-     * Calculate the rewards owed to 
+     * Calculate the rewards owed to a stakeholder.
+     * The interest will be calculated based on:
+     *  - The amount staked of the stakeholder
+     *  - The weight of the stakeholder
+     *  - The amount of interest periods staked
+     *  - The base and extra interest of the contract
+     * The formula of compounding interest is applied.
+     * @param withdrawl if true, send the rewards to the stakeholder.
+     *  if false, add the rewards to the staking balance of the stakeholder.
+     * @return reward The rewards of the stakeholder for previous periods.
      */
     function claimRewards(bool withdrawl) public returns(uint reward) {
 
@@ -160,9 +187,8 @@ contract EnvoyStaking is Ownable {
                 stakeholder.newStake = 0;
             }
 
+            // One period was already rewarded with old values
             stakeholder.interestDate += interestPeriod;
-            
-            // One period was already rewarded
             n-=1;
 
         }
@@ -208,5 +234,52 @@ contract EnvoyStaking is Ownable {
         return ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256(abi.encode(address(this), sender, weight))) , signature);
     }
 
+    /**
+     * Owner function to transfer the staking token from the contract
+     * address to the contract owner.
+     * The amount cannot exceed the amount staked by the stakeholders,
+     * making sure the funds of stakeholders stay in the contract.
+     * @param amount the amount to withraw as owner
+     */
+    function withdrawRemainingFunds(uint amount) public onlyOwner{
+        address sender = _msgSender();
+        
+        // Make sure the staked amounts are never withdrawn
+        if(amount > address(this).balance - totalStake){
+            amount = address(this).balance - totalStake;
+        }
+
+        stakingToken.transfer(sender, amount);
+    }
+
+    // List of owner functions to update the contract
+    function updateSignatureAddress(address value) public onlyOwner {
+        signatureAddress = value; 
+    }
+
+    function updateInterestDecimals(uint value) public onlyOwner{
+        interestDecimals = value;
+        emit ConfigUpdate('Decimals', value);
+    }
+
+    function updateBaseInterest(uint value) public onlyOwner{
+        baseInterest = value;
+        emit ConfigUpdate('Base interest', value);
+    }
+
+    function updateExtraInterest(uint value) public onlyOwner{
+        extraInterest = value;
+        emit ConfigUpdate('Extra interest', value);
+    }
+
+    function updateInterestPeriod(uint value) public onlyOwner{
+        interestPeriod = value;
+        emit ConfigUpdate('Interest period', value);
+    }
+
+    function updateCoolDownPeriod(uint value) public onlyOwner{
+        interestPeriod = value;
+        emit ConfigUpdate('Cool down period', value);
+    }
 
 }
