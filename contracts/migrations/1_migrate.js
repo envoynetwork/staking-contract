6f9d0948e4b19945c3c7ed6d58eeca77d00821da
Kasper De Blieck <KasperDeBlieck@hotmail.com>
2021-11-29 18:48:45 +0100

    refactor and improve structure of tests

--- contracts/migrations/1_migrate.js
+++ contracts/migrations/1_migrate.js
@@ -3,14 +3,12 @@ const TestToken = artifacts.require("TestToken");
 
 
 module.exports = async function (deployer, network, accounts) {
-  if(network === 'development'){
+  if(network === 'development' || network === 'rinkeby'){
     // Deploy a test token 
     const signatureAddress = '0xb9DD1FBbeB8f29DD181223DA36A397859B02834C' //29087b7eef74b57abe95659609151eeebac31ab7018a2a995a2f3bc1fdda89e3
     await deployer.deploy(TestToken)
     const token = await TestToken.deployed()
-    for(var account in accounts){
-      token.claim(accounts[account], web3.utils.toWei('100'))
-    }
     await deployer.deploy(EnvoyStaking, signatureAddress, token.address);
+    
   }
 };
