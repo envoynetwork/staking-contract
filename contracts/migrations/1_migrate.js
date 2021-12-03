5334d7a1e4e6627486f9f74fa7e4311175644806
Kasper De Blieck <KasperDeBlieck@hotmail.com>
2021-12-03 12:14:42 +0100

    use dotenv for env vars

--- contracts/migrations/1_migrate.js
+++ contracts/migrations/1_migrate.js
@@ -1,14 +1,22 @@
+
+require('dotenv').config({path: '../.env'})
 const EnvoyStaking = artifacts.require("EnvoyStaking");
 const TestToken = artifacts.require("TestToken");
 
-
 module.exports = async function (deployer, network, accounts) {
-  if(network === 'development' || network === 'rinkeby'){
-    // Deploy a test token 
-    const signatureAddress = '0xb9DD1FBbeB8f29DD181223DA36A397859B02834C' //29087b7eef74b57abe95659609151eeebac31ab7018a2a995a2f3bc1fdda89e3
+  
+  const signerKey = network === 'mainnet' ? process.env.PRODUCTION_SIGNATURE_KEY : process.env.DEVELOPMENT_SIGNATURE_KEY
+  const signatureAddress = web3.eth.accounts.privateKeyToAccount(signerKey).address
+
+  // Deploy a test token no token address is defined.
+  // Only deploy on dev or test nets.
+  var tokenAddress = process.env[network.toUpperCase()+'_TOKEN_ADDRESS']
+
+  if(tokenAddress === '' && network != 'mainnet'){
     await deployer.deploy(TestToken)
     const token = await TestToken.deployed()
-    await deployer.deploy(EnvoyStaking, signatureAddress, token.address);
-    
+    tokenAddress = token.address
   }
+  
+  await deployer.deploy(EnvoyStaking, signatureAddress, tokenAddress);    
 };
